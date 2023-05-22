ab7bcad907920c9727123ec18b271e2c4fc2f582
Buivanbi <dominicus.minh@gmail.com>
2023-05-22 18:33:20 +0700

    cc

---
+++ Tourist-Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tourist_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
